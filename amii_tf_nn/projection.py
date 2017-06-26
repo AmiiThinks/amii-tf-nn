dcf477c7a403598fb882ce6da067b7dfddeea08b
Dustin Morrill <dmorrill10@gmail.com>
2017-06-26 17:23:09 -0600

    Rename variable.

--- amii_tf_nn/projection.py
+++ amii_tf_nn/projection.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 
 
-def l1_projection_to_simplex(X, use_locking=False):
-    non_negative_regrets = tf.maximum(0.0, tf.convert_to_tensor(X))
+def l1_projection_to_simplex(tensor, use_locking=False):
+    non_negative_regrets = tf.maximum(0.0, tf.convert_to_tensor(tensor))
     strat = non_negative_regrets / tf.reduce_sum(non_negative_regrets, axis=0)
     return tf.where(
         tf.is_nan(strat),
