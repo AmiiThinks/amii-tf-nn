4afebcbeb1c2649feb2d44afe5cacd02ccb79598
Dustin Morrill <dmorrill10@gmail.com>
2017-07-04 13:46:42 -0600

    Generalize L1 projection to simplex to Tensors with uncertain dimensions.

--- amii_tf_nn/projection.py
+++ amii_tf_nn/projection.py
@@ -2,13 +2,13 @@ import tensorflow as tf
 
 
 def l1_projection_to_simplex(tensor):
-    non_negative_regrets = tf.maximum(0.0, tf.convert_to_tensor(tensor))
+    non_negative_regrets = tf.maximum(0.0, tensor)
     strat = non_negative_regrets / tf.reduce_sum(non_negative_regrets, axis=0)
     return tf.where(
         tf.is_nan(strat),
-        tf.fill(
-            strat.shape,
-            tf.reciprocal(tf.to_float(strat.shape[0]))
+        (
+            tf.zeros_like(strat) +
+            tf.reciprocal(tf.to_float(strat.shape[0].value))
         ),
         strat
     )
