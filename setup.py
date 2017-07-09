655ac367fde72d892eccfeece3ebf6719612d35b
Dustin Morrill <dmorrill10@gmail.com>
2017-07-08 22:14:49 -0600

    Add version to tf library reminder.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         'future == 0.15.2',
         'setuptools >= 20.2.2',
         'pyyaml == 3.12',
-        # tensorflow or tensorflow-gpu
+        # tensorflow or tensorflow-gpu v1.2
     ],
     tests_require=['pytest', 'pytest-cov'],
     setup_requires=['pytest-runner'],
