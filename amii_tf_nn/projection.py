f6d19249dc31406b91337c2a1d882989d87b69b6
Dustin Morrill <dmorrill10@gmail.com>
2017-11-13 11:37:54 -0700

    Add option for l1_projection_to_simplex to row normalize.

--- amii_tf_nn/projection.py
+++ amii_tf_nn/projection.py
@@ -1,14 +1,16 @@
 import tensorflow as tf
 
 
-def l1_projection_to_simplex(tensor):
-    non_negative_regrets = tf.maximum(0.0, tensor)
-    strat = non_negative_regrets / tf.reduce_sum(non_negative_regrets, axis=0)
+def l1_projection_to_simplex(tensor, row_normalize=False):
+    non_negative = tf.maximum(0.0, tensor)
+    if row_normalize:
+        non_negative = tf.transpose(non_negative)
+    fallback = 1.0 / non_negative.shape[0].value
+    projected = non_negative / tf.reduce_sum(non_negative, axis=0)
+    if row_normalize:
+        projected = tf.transpose(projected)
     return tf.where(
-        tf.is_nan(strat),
-        (
-            tf.zeros_like(strat) +
-            tf.reciprocal(tf.to_float(strat.shape[0].value))
-        ),
-        strat
+        tf.is_nan(projected),
+        tf.zeros_like(projected) + tf.to_float(fallback),
+        projected
     )
--- test/projection_test.py
+++ test/projection_test.py
@@ -37,6 +37,24 @@ class ProjectionTest(tf.test.TestCase):
                 ]
             )
 
+    def test_l1_multiple_rows_axis_1(self):
+        patient = l1_projection_to_simplex(
+            tf.constant(
+                [
+                    [2.0, 8.0, -5.0],
+                    [9.5, 0.4, 0.1]
+                ]
+            ),
+            row_normalize=True
+        )
+        with self.test_session():
+            self.assertAllClose(
+                patient.eval(),
+                [
+                    [0.2, 0.8, 0.0],
+                    [0.95, 0.04, 0.01]
+                ]
+            )
 
 if __name__ == '__main__':
     tf.test.main()
